1fb6fadcc493684ecef21c09ce81f42591b4bafe
Devansh Parapalli <parapallidev@gmail.com>
2024-02-16 23:41:53 +0530

    added some more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+
+    let data: null | any = null;
+
+    async function test_response() {
+        const res = await fetch('http://localhost:4945')
+        const _data = await res.json()
+        data = _data
+    }   
+
+</script>
+
+<h1>{(data) ? JSON.stringify(data):'Welcome to SvelteKit'}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="p-4 bg-white text-black" on:click={test_response} ></button>
