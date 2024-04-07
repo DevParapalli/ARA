4a2b1b3b844a6c595d767aa01a791453e2382de2
Devansh Parapalli <parapallidev@gmail.com>
2024-04-07 17:54:57 +0530

    added a somewhat simple page for screenshoting

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,219 @@
+<div class="flex max-w-[100vw] flex-col items-center justify-start xl:flex-row xl:items-start xl:justify-between">
+    <div class="shrink xl:w-1/2">
+        <div
+            class="flex items-center justify-center px-2 py-10 text-center xl:min-h-screen xl:justify-start xl:pe-0 xl:ps-10 xl:text-start">
+            <div>
+                <div class="h-4" />
+                <h1
+                    class="font-title [:root[dir=rtl]_&amp;]:leading-[1.35] text-center text-[clamp(2rem,6vw,4.2rem)] font-black leading-[1.1] [word-break:auto-phrase] xl:w-[115%] xl:text-start">
+                    <span
+                        class="[&amp;::selection]:text-base-content [&amp;::selection]:bg-blue-700/20 brightness-150 contrast-150"
+                        >The most powerful</span> <br />
+                    <span class="inline-grid"
+                        ><span
+                            class="pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text blur-xl [-webkit-text-fill-color:transparent] [transform:translate3d(0,0,0)] before:content-[attr(data-text)] [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                            aria-hidden="true"
+                            data-text="Research Assistant"></span>
+                        <span
+                            class="[&amp;::selection]:text-base-content [&amp;::selection]:bg-blue-700/20 relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                            >Research Assistant</span
+                        ></span> <br />
+                    <span
+                        class="[&amp;::selection]:text-base-content [&amp;::selection]:bg-blue-700/20 brightness-150 contrast-150"
+                        >ever.</span>
+                </h1>
+                <div class="h-4" />
+                <p class="font-title py-4 pr-4 text-justify font-light text-base-content/70 md:text-lg xl:text-xl">
+                    Enriching exploration, one prompt at a time.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="w-full shrink xl:w-1/2">
+        <div class="grid min-h-screen grid-cols-3 grid-rows-3 gap-6 p-2 xl:pt-10">
+            <div
+                class="col-span-2 h-full w-full rounded-box border-2 border-base-content/30 bg-base-200 p-4 hover:border-base-content/60">
+                <div class="group flex flex-row justify-between">
+                    <div class="flex w-full flex-col items-start justify-between gap-2 p-2 xl:w-1/2">
+                        <div class="rounded-md border-2 border-base-content/30 bg-base-content/10 p-2 text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                                ><path
+                                    fill="currentColor"
+                                    d="M3.05 13H1v-2h2.05C3.5 6.83 6.83 3.5 11 3.05V1h2v2.05c4.17.45 7.5 3.78 7.95 7.95H23v2h-2.05c-.45 4.17-3.78 7.5-7.95 7.95V23h-2v-2.05C6.83 20.5 3.5 17.17 3.05 13M12 5a7 7 0 0 0-7 7a7 7 0 0 0 7 7a7 7 0 0 0 7-7a7 7 0 0 0-7-7" /></svg>
+                        </div>
+                        <span class="text-xl uppercase">Auto-searching</span>
+                        <div class="text-justify">
+                            ARA autosearches the interwebs in relation to your prompt, saving you the hassle of
+                            going through multiple pages.
+                        </div>
+                    </div>
+                    <div class="hidden w-1/2 p-4 text-base-content/40 group-hover:text-base-content xl:flex">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            height="100%"
+                            class="rounded-box border-none border-base-content/30 group-hover:border-base-content"
+                            viewBox="0 0 48 48"
+                            ><g fill="none" stroke="currentColor" stroke-width="4"
+                                ><path stroke-linecap="round" d="M4 7h40M4 23h11M4 39h11" /><path
+                                    d="M31.5 34a8.5 8.5 0 1 0 0-17a8.5 8.5 0 0 0 0 17Z" /><path
+                                    stroke-linecap="round"
+                                    d="m37 32l7 7.05" /></g
+                            ></svg>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="row-span-2 h-full w-full rounded-box border-2 border-base-content/30 bg-base-200 p-4 hover:border-base-content/60">
+                <div class="group flex flex-col items-center justify-evenly gap-2">
+                    <div class="rounded-md border-2 border-base-content/30 bg-base-content/10 p-2 text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 32 32"
+                            ><path
+                                fill="currentColor"
+                                d="M16 32C7.163 32 0 24.837 0 16S7.163 0 16 0s16 7.163 16 16a16 16 0 0 1-16 16m0-30C8.268 2 2 8.268 2 16s6.268 14 14 14s14-6.268 14-14A14 14 0 0 0 16 2" /><path
+                                fill="currentColor"
+                                d="M23 15h-6V9h-2v6H9v2h6v6h2v-6h6z"
+                                class="ouiIcon__fillSecondary" /></svg>
+                    </div>
+
+                    <span class="text-xl uppercase">Text Generation</span>
+
+                    <p class="text-justify">
+                        ARA generates text according to your prompt, giving you interactivity over ARA's process.
+                        Use multiple models, multiple prompts, until you get your desired output, all from one,
+                        common interface.
+                    </p>
+
+                    <div class="hidden h-1/2 p-4 text-base-content/40 group-hover:text-base-content xl:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512"
+                            ><path
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="32"
+                                d="M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48" /><path
+                                fill="currentColor"
+                                d="M459.94 53.25a16.06 16.06 0 0 0-23.22-.56L424.35 65a8 8 0 0 0 0 11.31l11.34 11.32a8 8 0 0 0 11.34 0l12.06-12c6.1-6.09 6.67-16.01.85-22.38M399.34 90L218.82 270.2a9 9 0 0 0-2.31 3.93L208.16 299a3.91 3.91 0 0 0 4.86 4.86l24.85-8.35a9 9 0 0 0 3.93-2.31L422 112.66a9 9 0 0 0 0-12.66l-9.95-10a9 9 0 0 0-12.71 0" /></svg>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="central h-full w-full rounded-box border-2 border-base-content/30 bg-base-200 p-4 hover:border-base-content/60">
+                <div class="flex flex-col items-center p-2">
+                    <div class="rounded-md border-2 border-base-content/30 bg-base-content/10 p-2 text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"
+                            ><path
+                                fill="currentColor"
+                                d="M24 14.6c0 .6-1.2 1-2.6 1.2c-.9-1.7-2.7-3-4.8-3.9c.2-.3.4-.5.6-.8h.8c3.1-.1 6 1.8 6 3.5M6.8 11H6c-3.1 0-6 1.9-6 3.6c0 .6 1.2 1 2.6 1.2c.9-1.7 2.7-3 4.8-3.9zm5.2 1c2.2 0 4-1.8 4-4s-1.8-4-4-4s-4 1.8-4 4s1.8 4 4 4m0 1c-4.1 0-8 2.6-8 5c0 2 8 2 8 2s8 0 8-2c0-2.4-3.9-5-8-5m5.7-3h.3c1.7 0 3-1.3 3-3s-1.3-3-3-3c-.5 0-.9.1-1.3.3c.8 1 1.3 2.3 1.3 3.7c0 .7-.1 1.4-.3 2M6 10h.3C6.1 9.4 6 8.7 6 8c0-1.4.5-2.7 1.3-3.7C6.9 4.1 6.5 4 6 4C4.3 4 3 5.3 3 7s1.3 3 3 3" /></svg>
+                    </div>
+                    <span class="text-xl">Collaboration</span>
+                    <p class="w-full text-justify">
+                        [Coming Soon] ARA allows you to conduct research, have discussions and engage in
+                        conversation right within a notebook interface.
+                    </p>
+                </div>
+            </div>
+            <div
+                class="row-span-2 h-full w-full rounded-box border-2 border-base-content/30 bg-base-200 p-4 hover:border-base-content/60">
+                <div class="group flex flex-col items-center justify-evenly gap-2">
+                    <div class="rounded-md border-2 border-base-content/30 bg-base-content/10 p-2 text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 32 32"
+                            ><path
+                                fill="currentColor"
+                                d="M16 32C7.163 32 0 24.837 0 16S7.163 0 16 0s16 7.163 16 16a16 16 0 0 1-16 16m0-30C8.268 2 2 8.268 2 16s6.268 14 14 14s14-6.268 14-14A14 14 0 0 0 16 2" /><path
+                                fill="currentColor"
+                                d="M23 15h-6V9h-2v6H9v2h6v6h2v-6h6z"
+                                class="ouiIcon__fillSecondary" /></svg>
+                    </div>
+
+                    <span class="text-xl uppercase">Code Generation</span>
+
+                    <p class="text-justify">
+                        ARA contains state of the art models capable of understanding and applying code. Using the
+                        same prompt interface, ARA generates code samples utilizing the resources available on the
+                        web.
+                    </p>
+
+                    <div class="hidden h-1/2 p-4 text-base-content/40 group-hover:text-base-content xl:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 1024 1024"
+                            ><path
+                                fill="currentColor"
+                                d="M516 673c0 4.4 3.4 8 7.5 8h185c4.1 0 7.5-3.6 7.5-8v-48c0-4.4-3.4-8-7.5-8h-185c-4.1 0-7.5 3.6-7.5 8zm-194.9 6.1l192-161c3.8-3.2 3.8-9.1 0-12.3l-192-160.9A7.95 7.95 0 0 0 308 351v62.7c0 2.4 1 4.6 2.9 6.1L420.7 512l-109.8 92.2a8.1 8.1 0 0 0-2.9 6.1V673c0 6.8 7.9 10.5 13.1 6.1M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32m-40 728H184V184h656z" /></svg>
+                    </div>
+                </div>
+
+                <!--  -->
+            </div>
+            <div
+                class="col-span-2 h-full w-full rounded-box border-2 border-base-content/30 bg-base-200 p-4 hover:border-base-content/60">
+                <div class="group flex flex-row justify-between">
+                    <div class="flex w-full flex-col items-start justify-between gap-2 p-2 xl:w-1/2">
+                        <div class="rounded-md border-2 border-base-content/30 bg-base-content/10 p-2 text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"
+                                ><path
+                                    fill="currentColor"
+                                    d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8q0-.425-.288-.712T12 7q-.425 0-.712.288T11 8q0 .425.288.713T12 9m0 13q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.262 6.913T12 22m0-2.1q2.6-.825 4.3-3.3t1.7-5.5V6.375l-6-2.25l-6 2.25V11.1q0 3.025 1.7 5.5t4.3 3.3m0-7.9" /></svg>
+                        </div>
+                        <span class="text-xl uppercase">Bring your own runner</span>
+                        <div class="text-justify">
+                            <p>Worried about privacy ?</p>
+                             ARA can utilize local models, ensuring your data never leaves your device.
+                        </div>
+                    </div>
+                    <div class="hidden w-1/2 p-4 text-base-content/40 group-hover:text-base-content xl:flex">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            height="100%"
+                            class="rounded-box border-none border-base-content/30 group-hover:border-base-content"
+                            viewBox="0 0 48 48"
+                            ><path
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M17.753 18.688a4.634 4.634 0 0 1 9.269 0m0 0v2.789m-9.269-2.789v2.752" /><circle
+                                cx="22.387"
+                                cy="27.343"
+                                r="1.613"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" /><path
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M22.387 30.534v-1.528" /><rect
+                                width="12.848"
+                                height="12.848"
+                                x="15.963"
+                                y="21.098"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                rx="3.545" /><path
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M42.447 34.748A21.499 21.499 0 1 1 23.828 2.5" /></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+        *:not(svg *) {
+        @apply transition-colors duration-300;
+    }
+
+    .central {
+        grid-column: 2/3;
+        grid-row: 2/3;
+    }
+</style>
