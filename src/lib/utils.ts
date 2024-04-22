8c0e263a97ffea8d0ed4ad40ba90916ccc785388
Devansh Parapalli <parapallidev@gmail.com>
2024-04-22 17:05:25 +0530

    removed large message chunk stuff

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,18 @@
 import type { Tables } from './supabaseTypes';
 
+// function convert_cells_to_context(cells: Tables<'cells'>[]): string {
+//     let context = '';
+//     cells.forEach((cell) => {
+//         context += '<doc>' + `<id>${cell.id}</id>` + `<text>${cell.content}</text>` + '</doc>\n';
+//     });
+//     return context;
+// }
+
 function convert_cells_to_context(cells: Tables<'cells'>[]): string {
     let context = '';
-    cells.forEach((cell) => {
-        context += '<doc>' + `<id>${cell.id}</id>` + `<text>${cell.content}</text>` + '</doc>\n';
-    });
+    // cells.forEach((cell) => {
+        // context += '<doc>' + `<id>${cell.id}</id>` + `<text>${cell.content}</text>` + '</doc>\n';
+    // });
     return context;
 }
 
